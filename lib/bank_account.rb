52ff0ee7781d7dfdf34d1e1187c13f62fc0f5bb0
jaspercurry <jaspercurry@gmail.com>
2016-07-31 16:38:54 -0400

    build bank account and transfer classes

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,51 @@
 class BankAccount
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def name 
+    @name
+  end
+
+  def balance
+    @balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def balance=(balance)
+    @balance = balance
+  end
+ 
+
+  def status
+    @status
+  end
+
+  def status=(status)
+    @status = status
+  end
+
+  def deposit(amount)
+    self.balance= self.balance + amount
+  end
+
+  def valid?
+    if self.status == "open" && self.balance > 0
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    self.status="closed"
+  end
+
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,58 @@
 class Transfer
-  # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def status
+    @status
+  end
+
+  def sender
+    @sender
+  end
+
+  def receiver
+    @receiver
+  end
+
+  def amount
+    @amount
+  end
+
+  def status=(status)
+    @status = status
+  end 
+
+  def valid?
+    if self.sender.valid? && self.receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+
+    if self.status == "pending" && amount < sender.balance
+    self.sender.deposit(self.amount * -1)
+    self.receiver.deposit(self.amount)
+    self.status="complete"
+    else
+      self.status="rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+    self.sender.deposit(self.amount)
+    self.receiver.deposit(self.amount * -1)
+    self.status = "reversed"
+    else
+      "This transaction was never performed - please contact your bank for more information :)"
+    end
+  end
 end
